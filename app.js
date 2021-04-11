92d1525c615c1d67b692e1f25838dad6adb42429
Maikol Hernandez <maikol_hernandez_rm@hotmail.com>
2021-04-11 09:31:35 -0400

    Code challenge - arrays - remove undefined, null, false and 0

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ Arrays
 * Get higher array number in one iteration
 */
 
-const arr = [0, 10 ,67, -1500, 250, 1]
+// const arr = [0, 10 ,67, -1500, 250, 1]
 
 const getHigherNumber = (arr) => {
   let pivote = arr[0]
@@ -11,4 +11,19 @@ const getHigherNumber = (arr) => {
   return pivote
 }
 
-console.log(getHigherNumber(arr))
+// console.log(getHigherNumber(arr))
+
+/*
+Array
+* Remove undefined, null and 0 in one iteration
+*/
+
+const arr = [undefined, 0, null, false, 50, -450, {}]
+
+const cleanArray = (arr) => {
+  let result = []
+  arr.map(el => { if (el) result.push(el) })
+  return result
+}
+
+console.log(cleanArray(arr))
